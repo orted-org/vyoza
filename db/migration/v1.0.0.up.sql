1f331cc41656029675a87621d5f936a21c8a2d2b
himanshu-sah <itsmehs07@gmail.com>
2021-10-14 21:44:38 +0545

    updated .gitignore

--- db/migration/v1.0.0.up.sql
+++ db/migration/v1.0.0.up.sql
@@ -3,3 +3,15 @@ CREATE TABLE IF NOT EXISTS key_value_store (
     value_data TEXT NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE IF NOT EXISTS uptime_watch_list(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    location TEXT NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    interval INTEGER NOT NULL,
+    expected_status INTEGER NOT NULL,
+    retain_duration TEXT,
+    hook_level TEXT,
+    hook_addr TEXT
+);
