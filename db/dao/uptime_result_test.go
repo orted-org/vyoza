6e2570181e5227ab8d44c733c71c518ff8ead024
himanshu-sah <itsmehs07@gmail.com>
2021-10-18 09:46:34 +0545

    refactored code and added unit test for getting count for all the uptime result on id

--- db/dao/uptime_result_test.go
+++ db/dao/uptime_result_test.go
@@ -2,14 +2,14 @@ package db
 
 import (
 	"context"
+	"log"
 	"testing"
 
 	"github.com/orted-org/vyoza/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAddUptimeResult(t *testing.T) {
-
+func createRamdomUptimeResult(t *testing.T) UptimeResult {
 	uwr := createRandomUptimeWatchRequest(t)
 
 	arg := AddUptimeResultParams{
@@ -23,4 +23,17 @@ func TestAddUptimeResult(t *testing.T) {
 	require.NotZero(t, i.CreatedAt)
 	require.Equal(t, uwr.ID, i.ID)
 	require.Equal(t, i.ResponseTime, arg.ResponseTime)
+	return i
+}
+func TestAddUptimeResult(t *testing.T) {
+	createRamdomUptimeResult(t)
+}
+
+func TestGetUptimeResultCount(t *testing.T) {
+	i := createRamdomUptimeResult(t)
+	c, err := tq.GetUptimeResultCount(context.Background(), i.ID)
+	log.Println(c)
+	require.NoError(t, err)
+	require.NotEmpty(t, c)
+	require.NotZero(t, c)
 }
