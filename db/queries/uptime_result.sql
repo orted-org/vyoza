3c353057cece600f8bedd648fe7f3d12a38f18a5
himanshu-sah <itsmehs07@gmail.com>
2021-10-18 14:19:08 +0545

    added query for uptime result stats

--- db/queries/uptime_result.sql
+++ db/queries/uptime_result.sql
@@ -17,3 +17,40 @@ LIMIT ? OFFSET ?;
 -- delete uptime result 
 DELETE FROM uptime_result
 WHERE id = ?;
+-- get stats for an id
+SELECT uptime_result.id,
+    count(*) AS total_count,
+    count(
+        CASE
+            WHEN response_time <= uptime_watch_request.std_response_time THEN response_time
+        END
+    ) AS success_count,
+    count(
+        CASE
+            WHEN response_time > uptime_watch_request.std_response_time
+            AND response_time <= uptime_watch_request.max_response_time THEN response_time
+        END
+    ) AS warning_count,
+    count(
+        CASE
+            WHEN response_time > uptime_watch_request.max_response_time THEN response_time
+        END
+    ) AS error_count,
+    min(response_time) AS min_response_time,
+    max(response_time) AS max_response_time,
+    avg(
+        CASE
+            WHEN response_time <= uptime_watch_request.std_response_time THEN response_time
+        END
+    ) AS avg_success_resp_time,
+    avg(
+        CASE
+            WHEN response_time > uptime_watch_request.std_response_time
+            AND response_time <= uptime_watch_request.max_response_time THEN response_time
+        END
+    ) AS avg_warning_resp_time,
+    min(created_at) AS start_date,
+    max(created_at) AS end_date
+FROM uptime_result
+    INNER JOIN uptime_watch_request ON uptime_watch_request.id = uptime_result.id
+WHERE uptime_watch_request.id = ?;
