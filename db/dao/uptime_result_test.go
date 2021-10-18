e55a04eff1d7108f455d69cf140c6e487270db12
himanshu-sah <itsmehs07@gmail.com>
2021-10-18 10:37:05 +0545

    added test for deleting uptime result

--- db/dao/uptime_result_test.go
+++ db/dao/uptime_result_test.go
@@ -79,3 +79,48 @@ func TestGetUptimeResults(t *testing.T) {
 	require.Equal(t, avgSaved/n, avgCreated/n)
 
 }
+
+func TestDeleteUptimeResults(t *testing.T) {
+	n := 10
+	var allUR []UptimeResult
+	var savedRes []UptimeResult
+	var avgCreated = 0
+	var avgSaved = 0
+	uwr := createRandomUptimeWatchRequest(t)
+	for i := 0; i < n; i++ {
+		arg := AddUptimeResultParams{
+			ID:           uwr.ID,
+			ResponseTime: util.RandomInt(1, 10),
+		}
+		res, err := tq.AddUptimeResult(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, res)
+		allUR = append(allUR, res)
+		avgCreated = avgCreated + res.ResponseTime
+	}
+
+	err := tq.DeleteUptimeResults(context.Background(), uwr.ID)
+	require.NoError(t, err)
+
+	i := 0
+	for {
+		res, err := tq.GetUptimeResults(context.Background(), GetUptimeResultsParams{
+			ID:     uwr.ID,
+			Offset: i * 2,
+			Limit:  2,
+		})
+		require.NoError(t, err)
+		if len(res) == 0 {
+			break
+		}
+		savedRes = append(savedRes, res...)
+		for _, inv := range res {
+			avgSaved = avgSaved + inv.ResponseTime
+		}
+		i++
+	}
+
+	require.Len(t, allUR, n)
+	require.Len(t, savedRes, 0)
+	require.Equal(t, avgSaved/n, 0)
+}
