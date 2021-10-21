5e3b75e2fa011e6ce00fc5316b510b03f2f62541
himanshu-sah <itsmehs07@gmail.com>
2021-10-21 13:32:17 +0545

    updated query in .sql files

--- db/queries/uptime_watch_request.sql
+++ db/queries/uptime_watch_request.sql
@@ -6,15 +6,17 @@ INSERT INTO uptime_watch_request (
         enabled,
         enable_updated_at,
         interval,
+        sslMonitor,
         expected_status,
         std_response_time,
         max_response_time,
         retain_duration,
         hook_level,
         hook_addr,
-        hook_secret
+        hook_secret,
+        notificationEmail,
     )
-VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 RETURNING id,
     title,
     description,
@@ -28,7 +30,8 @@ RETURNING id,
     retain_duration,
     hook_level,
     hook_addr,
-    hook_secret;
+    hook_secret,
+    notificationEmail;
 -- get one uptime watch request by id
 SELECT id,
     title,
@@ -37,13 +40,15 @@ SELECT id,
     enabled,
     enable_updated_at,
     interval,
+    sslMonitor,
     expected_status,
     std_response_time,
     max_response_time,
     retain_duration,
     hook_level,
     hook_addr,
-    hook_secret
+    hook_secret,
+    notificationEmail
 FROM uptime_watch_request
 WHERE id = ?;
 -- get all uptime watch request
@@ -54,13 +59,15 @@ SELECT id,
     enabled,
     enable_updated_at,
     interval,
+    sslMonitor,
     expected_status,
     std_response_time,
     max_response_time,
     retain_duration,
     hook_level,
     hook_addr,
-    hook_secret
+    hook_secret,
+    notificationEmail
 FROM uptime_watch_request;
 -- delete uptime watch by id
 DELETE FROM uptime_watch_request
