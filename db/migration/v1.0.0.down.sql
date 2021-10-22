33395aff0cfa810e5d401dff779a5232fb10cdef
himanshu-sah <itsmehs07@gmail.com>
2021-10-22 23:13:58 +0545

    added query for migrating down the uptime_ssl_info table & updated uptime_conclusion test

--- db/migration/v1.0.0.down.sql
+++ db/migration/v1.0.0.down.sql
@@ -2,3 +2,4 @@ DROP TABLE IF EXISTS key_value_store;
 DROP TABLE IF EXISTS uptime_watch_request;
 DROP TABLE IF EXISTS uptime_result;
 DROP TABLE IF EXISTS uptime_conclusion;
+DROP TABLE IF EXISTS uptime_ssl_info;
