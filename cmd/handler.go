eb5940307e86f0c121bdb34c26c776d6690950dd
himanshu-sah <itsmehs07@gmail.com>
2021-10-27 11:12:01 +0545

    added url params for uptime routes

--- cmd/handler.go
+++ cmd/handler.go
@@ -4,7 +4,7 @@ import "github.com/go-chi/chi/v5"
 
 func initHandler(app *App, r *chi.Mux) {
 	r.Post("/uptime", app.handleCreateWatchReq)
-	r.Get("/uptime", app.handleGetWatchReq)
-	r.Put("/uptime", app.handleUpdateWatchReq)
-	r.Delete("/uptime", app.handleDeleteWatchReq)
+	r.Get("/uptime/{id}", app.handleGetWatchReq)
+	r.Put("/uptime/{id}", app.handleUpdateWatchReq)
+	r.Delete("/uptime/{id}", app.handleDeleteWatchReq)
 }
