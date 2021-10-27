2cc5c73d5be8535f35c749b9a934f1937f6e4dd7
himanshu-sah <itsmehs07@gmail.com>
2021-10-27 21:07:58 +0545

    watcher, quitter and logger init

--- cmd/init.go
+++ cmd/init.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	db "github.com/orted-org/vyoza/db/dao"
@@ -10,6 +11,8 @@ import (
 
 func initDB() (db.Store, error) {
 	var err error
+	
+	// TODO: pass db info from config
 	tDB, err := sql.Open("sqlite3", "../db.db")
 	if err != nil {
 		return nil, err
@@ -21,4 +24,33 @@ func initDB() (db.Store, error) {
 
 	// TODO: closing db connection
 	return q, nil
+}
+
+func initWatcher(app *App) {
+
+	// adding a quitter for uptime request watcher
+	app.quitters["watcher"] = make(chan struct{})
+
+	app.logger.Println("starting uptime request(ssl) watcher")
+	// TODO: add logic to send data to web socket clients
+	for {
+		select {
+		case data := <-app.watcher.Result:
+			app.store.AddUptimeResult(context.Background(), db.AddUptimeResultParams{
+				UWRID:        data.ID,
+				ResponseTime: data.ResponseTime,
+				Remark:       data.Remark,
+			})
+		case sslData := <-app.watcher.SSLResult:
+			app.store.UpdateUptimeSSLInfoByUWRID(context.Background(), db.UptimeSSLInfo{
+				UWRID:      sslData.ID,
+				IsValid:    sslData.IsValid,
+				ExpiryDate: sslData.ExpiryDate,
+				Remark:     sslData.Remark,
+				UpdatedAt:  time.Now().UTC(),
+			})
+		case <-app.quitters["watcher"]:
+			app.logger.Println("quitting uptime request(ssl) watcher")
+		}
+	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	db "github.com/orted-org/vyoza/db/dao"
@@ -13,9 +14,22 @@ type App struct {
 	// db store
 	store db.Store
 
+	//logger
+	logger *log.Logger
+
 	// uptime and ssl watcher
-	watcher watcher.Watcher
+	watcher *watcher.Watcher
+
+	// service quitter signal channel map
+	quitters map[string]chan struct{}
+
+	osSignal chan os.Signal
 }
+
+var (
+	lo = log.New(os.Stdout, "",
+		log.Ldate|log.Ltime|log.Lshortfile)
+)
 
 func main() {
 	store, err := initDB()
@@ -24,11 +38,15 @@ func main() {
 		return
 	}
 	app := &App{
-		store: store,
+		store:    store,
+		watcher:  watcher.New(),
+		quitters: make(map[string]chan struct{}),
+		logger:   lo,
 	}
 
 	r := chi.NewRouter()
 	initHandler(app, r)
+	initWatcher(app)
 
 	// TODO: server handling
 	srv := http.Server{
