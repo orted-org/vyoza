fb0b7513e1181e4588ebeff386faadfeb8bc8638
himanshu-sah <itsmehs07@gmail.com>
2021-10-27 15:39:51 +0545

    create watch req handler init

--- cmd/uptime_ssl_watch.go
+++ cmd/uptime_ssl_watch.go
@@ -1,9 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	db "github.com/orted-org/vyoza/db/dao"
+)
 
 func (app *App) handleCreateWatchReq(rw http.ResponseWriter, r *http.Request) {
-
+	var arg db.AddUptimeWatchRequestParams
+	err := getBody(r, &arg)
+	if err != nil {
+		sendErrorResponse(rw, http.StatusInternalServerError, nil, err.Error())
+		return
+	}
+	i, err := app.store.AddUptimeWatchRequest(r.Context(), arg)
+	if err != nil {
+		sendErrorResponse(rw, http.StatusInternalServerError, nil, err.Error())
+		return
+	}
+	sendResponse(rw, http.StatusCreated, i, "added to uptime watch request")
 }
 func (app *App) handleUpdateWatchReq(rw http.ResponseWriter, r *http.Request) {
 
