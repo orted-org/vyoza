a81d6d1a9d288eef101975fd2f3bef59cc5b8806
himanshu-sah <itsmehs07@gmail.com>
2021-11-15 22:31:00 +0545

    merged crypto and bcrypt file

--- util/crypto_test.go
+++ util/crypto_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestSHA256(t *testing.T) {
@@ -12,4 +13,18 @@ func TestSHA256(t *testing.T) {
 	err := CompareSHA256(text, sha256)
 	require.NotEmpty(t, text)
 	require.NoError(t, err)
+}
+
+func TestBcrypt(t *testing.T) {
+	secret := RandomString(8)
+	hs, err := HashSecret(secret)
+	require.NoError(t, err)
+	require.NotEmpty(t, hs)
+
+	err = VerifyHashSecret(secret, hs)
+	require.NoError(t, err)
+
+	err = VerifyHashSecret(RandomString(8), hs)
+	require.Error(t, err)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
 }
