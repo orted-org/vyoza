11ea6b9231a7c9e8694d8ffca99c8c0c62ed4f1c
himanshu-sah <itsmehs07@gmail.com>
2021-12-10 19:50:57 +0545

    added handler behind auth for certain endpoints

--- cmd/http-server/handler.go
+++ cmd/http-server/handler.go
@@ -9,21 +9,26 @@ import (
 func initHandler(app *App, r *chi.Mux) {
 
 	// uptime
-	r.Post("/uptime", app.handleCreateWatchReq)
-	r.Get("/uptime", app.handleGetWatchReq)
-	r.Put("/uptime/{id}", app.handleUpdateWatchReq)
-	r.Delete("/uptime/{id}", app.handleDeleteWatchReq)
+	r.Post("/uptime", app.handleCheckAllowance(http.HandlerFunc(app.handleCreateWatchReq)))
+	r.Get("/uptime", app.handleCheckAllowance(http.HandlerFunc(app.handleGetWatchReq)))
+	r.Put("/uptime/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleUpdateWatchReq)))
+	r.Delete("/uptime/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleDeleteWatchReq)))
 
 	// vault
 	r.Get("/vault/{name}", app.handleGetVault)
-	r.Post("/vault", app.handleSetVault)
-	r.Put("/vault", app.handleUpdateVault)
-	r.Delete("/vault/{name}", app.handleDeleteVault)
+	r.Post("/vault", app.handleCheckAllowance(http.HandlerFunc(app.handleSetVault)))
+	r.Put("/vault", app.handleCheckAllowance(http.HandlerFunc(app.handleUpdateVault)))
+	r.Delete("/vault/{name}", app.handleCheckAllowance(http.HandlerFunc(app.handleDeleteVault)))
 
 	// config store
 	r.Get("/cs/{name}", app.handleGetConfig)
 	r.Post("/cs", app.handleSetConfig)
 	r.Delete("/cs/{name}", app.handleDeleteConfig)
+
+	// services
+	r.Post("/service", app.handleCheckAllowance(http.HandlerFunc(app.handleCreateService)))
+	r.Delete("/service/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleDeleteService)))
+	r.Get("/service/{id}", app.handleCheckAllowance(http.HandlerFunc(app.handleGetService)))
 
 	// auth service
 	r.Post("/auth", app.handleLogin)
